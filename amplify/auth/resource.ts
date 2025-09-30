0ad829afc1c2c4de60792d3b66130a98c48c3824
sam1610 <samir.amri@gmail.com>
2025-09-30 21:56:06 +0300

    update data

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,26 +1,11 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
 
-// Define the schema for the Product model
-const schema = a.schema({
-  Product: a.model({
-    name: a.string().required(),
-    description: a.string(),
-    price: a.float().required(),
-    inStock: a.boolean().default(true),
-  })
-  .authorization(allow => [allow.publicApiKey()]), // Use API Key for public access
-});
-
-// This is a TypeScript-only export for creating a type-safe client
-export type Schema = ClientSchema<typeof schema>;
-
-// Define the data resource
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
-    apiKeyAuthorizationMode: {
-      expiresInDays: 365,
-    }
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
   },
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,53 +1,26 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
+// Define the schema for the Product model
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  Product: a.model({
+    name: a.string().required(),
+    description: a.string(),
+    price: a.float().required(),
+    inStock: a.boolean().default(true),
+  })
+  .authorization(allow => [allow.publicApiKey()]), // Use API Key for public access
 });
 
+// This is a TypeScript-only export for creating a type-safe client
 export type Schema = ClientSchema<typeof schema>;
 
+// Define the data resource
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 365,
+    }
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
