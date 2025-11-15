cc23b0051f1be52f77b1de49d14a0886ba817c36
sam1610 <samir.amri@gmail.com>
2025-11-15 19:28:39 +0300

    update amplify_output.json without command npx ampx sandbox

--- src/DataHook/amplifyClient.js
+++ src/DataHook/amplifyClient.js
@@ -2,5 +2,4 @@ import { generateClient } from 'aws-amplify/data';
 
 // 1. Generate the client a single time.
 export const client = generateClient({ authMode: 'userPool' });
-
 // 2. Export it so all other components can import it.
