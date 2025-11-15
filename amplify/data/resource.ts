8d7aceac5700047ac0449d0460c89138c8588417
sam1610 <samir.amri@gmail.com>
2025-11-15 15:21:54 +0300

    update resource   and amplifyClient userPool

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -80,7 +80,7 @@ export const data = defineData({
   schema,
   authorizationModes: {
     // ✅ UPDATED: Changed default authorization mode to API Key
-    defaultAuthorizationMode: 'apiKey',
+    defaultAuthorizationMode: 'userPool',
     apiKeyAuthorizationMode: {
       description: 'API Key for WhatsApp Flow Lambda',
       expiresInDays: 365 
--- src/DataHook/amplifyClient.js
+++ src/DataHook/amplifyClient.js
@@ -1,6 +1,6 @@
 import { generateClient } from 'aws-amplify/data';
 
 // 1. Generate the client a single time.
-export const client = generateClient({ authMode: 'apiKey' });
+export const client = generateClient({ authMode: 'userPool' });
 
 // 2. Export it so all other components can import it.
