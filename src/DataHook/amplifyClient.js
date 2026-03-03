edde2e57e71ce3da45d56dd7e02e9aea549eb184
sam1610 <samir.amri@gmail.com>
2026-03-03 15:13:26 +0300

    update amplifyClient ts

--- src/DataHook/amplifyClient.js
+++ src/DataHook/amplifyClient.js
@@ -2,11 +2,19 @@
 
 // export const client = generateClient();
 
-import { generateClient } from "aws-amplify/api";
-import { createAIHooks } from "@aws-amplify/ui-react-ai"; // 👈 New Import
+// import { generateClient } from "aws-amplify/api";
+// import { createAIHooks } from "@aws-amplify/ui-react-ai"; // 👈 New Import
 
-// 1. Create the standard Data Client
+// // 1. Create the standard Data Client
+// export const client = generateClient({ authMode: 'userPool' });
+
+// // 2. Generate the AI Hooks from that client
+// export const { useAIGeneration, useAIConversation } = createAIHooks(client);
+
+import { generateClient } from "aws-amplify/data";
+import { createAIHooks } from "@aws-amplify/ui-react-ai";
+
+// Standard JavaScript client (No <Schema> required)
 export const client = generateClient({ authMode: 'userPool' });
 
-// 2. Generate the AI Hooks from that client
 export const { useAIGeneration, useAIConversation } = createAIHooks(client);
