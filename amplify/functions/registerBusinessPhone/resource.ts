1bec1ad70b4641052aeb8eded81b4e28184f7ccd
sam1610 <samir.amri@gmail.com>
2026-04-29 13:41:15 +0300

    update ResourceGroupName @ The  resource.ts

--- amplify/functions/registerBusinessPhone/resource.ts
+++ amplify/functions/registerBusinessPhone/resource.ts
@@ -4,12 +4,11 @@ export const registerBusinessPhone = defineFunction({
   name: 'registerBusinessPhone',
   entry: './handler.ts',
   timeoutSeconds: 30, // Meta API calls can take a few seconds
+  resourceGroupName: 'data',
   environment: {
     // This tells Amplify to pull the secure secrets and inject them as process.env variables
     WABA_ID: secret('WABA_ID'),
     META_SYSTEM_USER_TOKEN: secret('META_SYSTEM_USER_TOKEN'),
-    PUBLIC_KEY: secret('PUBLIC_KEY'),
-    APPSYNC_ENDPOINT_URL: secret('APPSYNC_ENDPOINT_URL'),
-    APPSYNC_API_KEY: secret('APPSYNC_API_KEY')
+    PUBLIC_KEY: secret('PUBLIC_KEY')
   }
 });
